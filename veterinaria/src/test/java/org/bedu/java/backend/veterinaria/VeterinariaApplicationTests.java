97f7912d01f83c020cb329e49886517fdc08b9ff
Eduardo Zambrano <je99.zambrano@gmail.com>
2023-11-23 21:40:59 -0600

    Estructura del proyecto inicial

---
+++ veterinaria/src/test/java/org/bedu/java/backend/veterinaria/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
