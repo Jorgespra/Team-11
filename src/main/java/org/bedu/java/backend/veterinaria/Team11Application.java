e6e35d69954559cee2a6ab38899c8314ce92b316
“Jorgespra” <jorgespra@gmail.com>
2023-11-26 21:46:06 -0600

    modulo citas

---
+++ src/main/java/org/bedu/java/backend/veterinaria/Team11Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team11Application.class, args);
+	}
+
+}
---
+++ src/main/java/org/bedu/java/backend/veterinaria/repository/CitaRepository.java
@@ -0,0 +1,65 @@
+package org.bedu.java.backend.veterinaria.repository;
+
+import org.bedu.java.backend.veterinaria.model.Cita;
+import org.springframework.stereotype.Repository;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CitaRepository {
+
+    private long currentId;
+    private List<Cita> cita;
+
+    public CitaRepository(){
+        currentId = 1;
+        cita = new LinkedList<>();
+    }
+
+    public List<Cita> getAll(){
+        return  cita;
+    }
+
+
+    public Cita save (Cita data){
+        data.setId(++currentId);
+        cita.add(data);
+        return data;
+    }
+
+    public void update(long id, Cita data) {
+        Optional<Cita> entry = getById(id);
+
+        if (!entry.isPresent()) {
+            return;
+        }
+
+        Cita cita = entry.get();
+
+        if (data.getVetName() != null) {
+            cita.setVetName(data.getVetName());
+        }
+
+        if (data.getPetName() != null) {
+            cita.setPetName(data.getPetName());
+        }
+
+        if (data.getDueAppointment() !=null) {
+            cita.setDueAppointment(data.getDueAppointment());
+        }
+    }
+    public Optional<Cita> getById(long id) {
+        // Version junior
+        for (Cita cita : cita) {
+            if (cita.getId() == id) {
+                return Optional.of(cita);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+
+}
