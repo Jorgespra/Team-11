788f0916b4e87d845f50bb83de8f93ed55f6c886
Eduardo Zambrano <je99.zambrano@gmail.com>
2023-12-10 22:06:38 -0600

    se añadio modificaciones

--- veterinaria/src/main/java/org/bedu/java/backend/veterinaria/model/HistorialMedico.java
+++ veterinaria/src/main/java/org/bedu/java/backend/veterinaria/model/HistorialMedico.java
@@ -2,7 +2,6 @@ package org.bedu.java.backend.veterinaria.model;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
